bfe96d75ba3a7f06088ad3fbe874294b0d4a4159
Waine Oliveira Jr <waine@aerosim.io>
2024-12-08 16:23:28 -0300

    updated 04

--- 04_funcs/src/main.zig
+++ 04_funcs/src/main.zig
@@ -1,16 +1,28 @@
 const std = @import("std");
-const mod = @import("mod.zig");
+const module = @import("mod.zig");
 
 pub fn main() !void {
-    const my_result = sum_vals(10, 30);
+    const a: i32 = 10;
+    const my_result = sum_vals(a, 30);
 
     std.debug.print("my_result: {}\n", .{my_result});
+
+    var modifieble_var: i32 = 0;
+    std.debug.print("modifiable_var: {}\n", .{modifieble_var});
+    modify_reference(&modifieble_var);
+    std.debug.print("modifiable_var: {}\n", .{modifieble_var});
+
+    module.pub_function();
+    const mod_const = module.exposed_const;
+    std.debug.print("mod_const: {}\n", .{mod_const});
 }
 
+/// Sintaxe de uma função
+/// [<classificador>] fn <nome da função>(<nome argumento>: <tipo argumento>, ...) <tipo retorno>
 fn sum_vals(a: i32, b: i32) i32 {
     return a + b;
 }
 
 fn modify_reference(a: *i32) void {
-    a.* = 40;
+    a.* = 42;
 }
--- 04_funcs/src/mod.zig
+++ 04_funcs/src/mod.zig
@@ -4,6 +4,8 @@ fn private_function() void {
     std.debug.print("só módulo pode chamar funções privadas (sem 'pub')\n", .{});
 }
 
+pub const exposed_const = 10;
+
 pub fn pub_function() void {
     std.debug.print("Estou na função pública\n", .{});
     private_function();
